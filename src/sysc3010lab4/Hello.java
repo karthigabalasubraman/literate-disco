e381699276903a975a8dc871f8f0282b494383b9
karthigabalasubraman <karthigabalasubraman@cb5109-55.labs.sce.carleton.ca>
2017-10-02 14:04:12 -0400

    wqert

---
+++ src/sysc3010lab4/Hello.java
@@ -0,0 +1,22 @@
+
+
+public class Hello {
+
+	public static main(String[] args) 
+	{
+		Hello hello = new Hello();
+		hello.methodA();
+		hello.methodB();
+		hello.methodC();
+	}
+
+	public void methodA(void ) { }
+
+	public void methodb(void) {
+		int a =1;
+		int b = 12; //tst comment
+		//new comment
+	}
+
+	public void methodC(void) { }
+}
